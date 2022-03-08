42b74d20207e08e4c033d33f5003d916d90cd1e4
Jennifer <rosatuttie@gmail.com>
2022-03-08 13:14:11 -0300

    segundo commit, trabalhando nos botoes scroll lateral

--- lib/barraApp.dart
+++ lib/barraApp.dart
@@ -1,67 +1,55 @@
 import 'package:flutter/material.dart';
 
 class BarraApp extends StatelessWidget {
-  const BarraApp({ Key? key }) : super(key: key);
+  const BarraApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return PreferredSize(
-        preferredSize: Size.fromHeight(80),
-        child: AppBar(
-          centerTitle: false,
-          automaticallyImplyLeading: false,
-          backgroundColor: Colors.white,
-          leading: Builder(builder: (BuildContext context) {
-            return Padding(
-              padding: const EdgeInsets.only(left: 8, top: 9, right: 6),
-              child: IconButton(
-                icon: Image.asset('assets/images/menu.png'),
-                color: Colors.black,
-                onPressed: () {},
-              ),
-            );
-          }),
-          title: Padding(
-            padding: const EdgeInsets.only(top: 10.0, left: 70.0),
-            child: Column(
-              children: <Widget>[
-                Text(
-                  'Location',
-                  style: TextStyle(
-                      color: Colors.black.withOpacity(0.5), fontSize: 15),
-                ),
-                const Text(
-                  'Cameron St. Boston',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 15,
-                  ),
-                ),
-              ],
+      preferredSize: const Size.fromHeight(80),
+      child: AppBar(
+        centerTitle: false,
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.white,
+        leading: Builder(builder: (BuildContext context) {
+          return Padding(
+            padding: const EdgeInsets.only(left: 8, top: 9, right: 6),
+            child: IconButton(
+              icon: Image.asset('assets/images/icons/menu1.png'),
+              color: Colors.black,
+              onPressed: () {},
             ),
-          ),
-          actions: [
-            Padding(
-              padding: const EdgeInsets.only(top: 8.0, right: 10.0),
-              child: Container(
-                width: 55.0,
-                height: 100.0,
-                decoration: BoxDecoration(
-                  image: const DecorationImage(
-                    image: NetworkImage(
-                        'https://scontent.fbjp1-1.fna.fbcdn.net/v/t39.30808-6/274006977_5263251960364989_7689438607282306194_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeFwuvHcGU2sJh19dnUGKnZeeXWZ4NLItWN5dZng0si1Y7hvvP_G14Z7FkF4OevScCr2FqQx-lqYHNRzIzO1Du2o&_nc_ohc=b4kWpGkTb20AX_wmzwu&_nc_ht=scontent.fbjp1-1.fna&oh=00_AT_Ns3WRAqD3r5-5Njj76VGUUA08WTv714KpfB6JDdbuig&oe=622C608A'),
-                        fit: BoxFit.cover,
-                  ),
-                  borderRadius: const BorderRadius.all(Radius.circular(50.0)),
-                  border: Border.all(
-                    color: Colors.transparent,
-                    width: 4.0
-                  ),
+          );
+        }),
+        title: Padding(
+          padding: const EdgeInsets.only(top: 10.0, left: 70.0),
+          child: Column(
+            children: <Widget>[
+              Text(
+                'Location',
+                style: TextStyle(
+                    color: Colors.black.withOpacity(0.5), fontSize: 15),
+              ),
+              const Text(
+                'Cameron St. Boston',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 15,
                 ),
               ),
-            )
-          ],
+            ],
+          ),
         ),
-      );
+        actions: const [
+          Padding(
+            padding: EdgeInsets.only(top: 10),
+            child: CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('assets/images/person/avatar.jpg'),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -6,11 +6,105 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: PreferredSize(
+    return Scaffold(
+      appBar: const PreferredSize(
         preferredSize: Size.fromHeight(80),
         child: BarraApp(),
       ),
+      body: Stack(children: [
+        Container(
+          height: 80,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 25),
+                child: CircleAvatar(
+                  radius: 25,
+                  backgroundColor: Colors.white,
+                  child: IconButton(
+                    icon: Image.asset('assets/images/icons/settings.png'),
+                    onPressed: () {},
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 15, top: 15, bottom: 15),
+                child: ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: Image.asset('assets/images/icons/dog.png'),
+                  label: const Text('Dogs'),
+                  style: ButtonStyle(
+                    backgroundColor: MudarCor(Colors.white, Colors.red),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),
+                      ),
+                    ),
+                    overlayColor: MudarCor(
+                        Colors.white, const Color.fromARGB(255, 216, 95, 47)),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 15, top: 15, bottom: 15),
+                child: ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: Image.asset('assets/images/icons/cat.png'),
+                  label: const Text(
+                    'Cats',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor: MudarCor(Colors.white, Colors.red),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),
+                      ),
+                    ),
+                    overlayColor: MudarCor(
+                        Colors.white, const Color.fromARGB(255, 216, 95, 47)),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 15, top: 15, bottom: 15),
+                child: ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: Image.asset('assets/images/icons/bird.png'),
+                  label: const Text(
+                    'Birds',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor: MudarCor(Colors.white, Colors.red),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),
+                      ),
+                    ),
+                    overlayColor: MudarCor(
+                        Colors.white, const Color.fromARGB(255, 216, 95, 47)),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ]),
     );
   }
+}
+
+// ignore: non_constant_identifier_names
+MaterialStateProperty<Color> MudarCor(Color cor, Color corAoApertar) {
+  // ignore: prefer_function_declarations_over_variables
+  final mudarCor = (Set<MaterialState> states) {
+    if (states.contains(MaterialState.pressed)) {
+      return corAoApertar;
+    } else {
+      return cor;
+    }
+  };
+  return MaterialStateProperty.resolveWith(mudarCor);
 }
